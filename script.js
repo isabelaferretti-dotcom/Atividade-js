f4720d4c5296670bf6559e28df55a8bf54bb44dd
ISABELA MONTEIRO DRUMOND FERRETTI <isabela_ferretti@estudante.sesisenai.org.br>
2026-08-13 21:24:07 -0300

    fiz a 7

--- script.js
+++ script.js
@@ -68,7 +68,10 @@ document.getElementById("btn6").addEventListener("click", () => {
 // e mostre dentro do #resultado7
 //
 // Dica: use .value para pegar o texto do input
-
+document.getElementById("btn7").addEventListener("click", function() {
+    document.getElementById("resultado7").textContent =
+        document.getElementById("input7").value;
+});
 
 
 // ---------- Exercicio 8 ----------
